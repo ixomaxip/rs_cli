c4b206e41e30c0c2f591d65b61656d5a76227b4a
ixomaxip <ixomaxip@gmail.com>
2022-01-19 21:51:55 +0500

    argparse

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,15 @@
+
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf
+}
+
 fn main() {
     println!("Hello, world!");
+    let pattern = std::env::args().nth(1).expect("no pattern provided");
+    let path = std::env::args().nth(2).expect("no path provided");
+    let args = Cli {
+        pattern: pattern,
+        path: std::path::PathBuf::from(path)
+    };
 }
