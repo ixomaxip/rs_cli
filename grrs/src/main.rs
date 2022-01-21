1b41f8724b81a1212fe504cd90e09d01dc47138f
ixomaxip <ixomaxip@gmail.com>
2022-01-22 00:21:24 +0500

    replace panic with return error

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,6 +1,5 @@
 use std::io::{self, BufReader, BufRead};
 use std::fs::File;
-use std::panic;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -10,18 +9,19 @@ struct Cli {
     path: std::path::PathBuf
 }
 
+// fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn main() -> io::Result<()> {
     let args = Cli::parse();
     let file = match File::open(&args.path) {
         Ok(f) => f,
-        Err(error) => panic!("could not read file: {:?}", error)
+        Err(error) => return Err(error.into())
     };
     let reader = BufReader::new(file);   
     
     for line in reader.lines() {
         let line = match line {
             Ok(l) => l,
-            Err(error) => { println!("line error: {:?}", error); "".to_string()}
+            Err(error) => { println!("line error: {:?}", error); "".to_string() }
         };
         if line.contains(&args.pattern) {
             print!("{}", line);
