2f8d40cc4c9d3a0aee2c5fc1db97bd69a22f39e5
ixomaxip <ixomaxip@gmail.com>
2022-01-22 02:16:27 +0500

    add logger

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -2,6 +2,8 @@ use std::io::{BufReader, BufRead};
 use std::fs::File;
 use clap::Parser;
 use anyhow::{Context, Result};
+use log::{info, warn};
+use env_logger;
 
 #[derive(Parser, Debug)]
 struct Cli {
@@ -14,18 +16,23 @@ struct Cli {
 struct CustomError(String);
 // fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn main() -> Result<()> {
+    env_logger::init();
+    
     let args = Cli::parse();
     let path = args.path.to_str()
         .with_context(|| "bad path")?;
+    
     let file = File::open(path)
         .with_context(|| format!("could not read file '{}'", path))?;
+    
+    info!("read file {}", path);
     
     let reader = BufReader::new(file);
     
     for line in reader.lines() {
         let line = match line {
             Ok(l) => l,
-            Err(error) => { println!("line error: {:?}", error); "".to_string() }
+            Err(error) => { warn!("line error: {:?}", error); "".to_string() }
         };
         if line.contains(&args.pattern) {
             println!("{}", line);
