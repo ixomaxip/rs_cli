4be7e9442eaa52c0d4bc4c54159017145a02f153
ixomaxip <ixomaxip@gmail.com>
2022-01-22 21:51:56 +0500

    move old test to new loc

--- grrs/tests/cli.rs
+++ grrs/tests/cli.rs
@@ -13,3 +13,27 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
         .stderr(predicate::str::contains("could not read file"));
     Ok(())
 }
+
+// #[test]
+// fn test() {
+//     let result = 42;
+//     let expected = grrs::answer();
+//     assert_eq!(expected, result);
+// }
+
+// use tempfile::tempdir;
+// use std::io::Write;
+// #[test]
+// #[ignore]
+// fn find_a_match() -> Result<()>{
+//     let dir = tempdir()?;
+//     let filepath = dir.path().join("find_matches_test.txt");
+//     let mut file = File::create(filepath)?;
+//     write!(file, "lorem ipsum\ndolor sit amet")?;
+//     let reader = BufReader::new(file);
+//     let mut result = Vec::new();
+//     grrs::find_matches(reader, "lorem", &mut result)?;
+//     assert_eq!(result, b"lorem ipsum");   
+
+//     Ok(())    
+// }
