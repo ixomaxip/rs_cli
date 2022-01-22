26aeb1c941a4fc7d8262017620612bfaa7e3b79c
ixomaxip <ixomaxip@gmail.com>
2022-01-22 20:03:53 +0500

    nonworking test

--- grrs/tests/grrs.rs
+++ grrs/tests/grrs.rs
@@ -5,3 +5,20 @@ fn test() {
     assert_eq!(expected, result);
 }
 
+
+// use tempfile::tempdir;
+// use std::io::Write;
+// #[test]
+// #[ignore]
+// fn find_a_match() -> Result<()>{
+//     let dir = tempdir()?;
+//     let filepath = dir.path().join("find_matches_test.txt");
+//     let mut file = File::create(filepath)?;
+//     write!(file, "lorem ipsum\ndolor sit amet")?;
+//     let reader = BufReader::new(file);
+//     let mut result = Vec::new();
+//     grrs::find_matches(reader, "lorem", &mut result)?;
+//     assert_eq!(result, b"lorem ipsum");   
+    
+//     Ok(())    
+// }
