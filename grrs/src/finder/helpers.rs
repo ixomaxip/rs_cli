cfb223794ab924705c8c8fd3ec258e7d70613e31
ixomaxip <ixomaxip@gmail.com>
2022-01-23 16:16:44 +0500

    move find_matches to module subdir

---
+++ grrs/src/finder/helpers.rs
@@ -0,0 +1,17 @@
+use anyhow::Result;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use log::{warn};
+
+pub fn find_matches(reader: BufReader<File>, pattern: &str, mut writer: impl std::io::Write) -> Result<()> {
+    for (idx, line) in reader.lines().enumerate() {
+        let line = match line {
+            Ok(l) => l,
+            Err(error) => { warn!("line error: {:?}", error); "".to_string() }
+        };
+        if line.contains(pattern) {
+            writeln!(writer, "{}\t{}", idx, line)?;
+        }
+    }
+    Ok(())
+}
